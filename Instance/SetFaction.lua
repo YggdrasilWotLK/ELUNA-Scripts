4ecbd48113cef85b68bedfe0af2e2b0fd5109897
Yggdrasil WoW <157967573+YggdrasilWotLK@users.noreply.github.com>
2024-08-07 06:05:25 +0200

    Update SetFaction.lua
    Update to only activate on raids and instances without full AzerothCore cross-faction support

--- Instance/SetFaction.lua
+++ Instance/SetFaction.lua
@@ -5,27 +5,34 @@ local cachedFaction = {}
 
 local function OnPlayerMapChange(event, player)
     local instanceId = player:GetInstanceId()
-    
-    if instanceId and instanceId > 0 then
-        if not cachedFaction[instanceId] then
-            for _, otherPlayer in pairs(GetPlayersInWorld()) do
-                if otherPlayer:GetInstanceId() == instanceId and player:GetGUIDLow() ~= otherPlayer:GetGUIDLow() then -- Builds cache if there is none, in the event of an ELUNA reload
-                    cachedFaction[instanceId] = otherPlayer:GetFaction()
-                    if player:GetFaction() ~= cachedFaction[instanceId] then
-                        player:SetFaction(cachedFaction[instanceId])
-                    end
-                    break
-                end
-            end
-            
-            if not cachedFaction[instanceId] then
-                cachedFaction[instanceId] = player:GetFaction()
-            end
-        else
-            if player:GetFaction() ~= cachedFaction[instanceId] then
-                player:SetFaction(cachedFaction[instanceId])
-            end
-        end
+    local mapId = player:GetMapId()
+	
+	if mapId == 631 or mapId == 658 
+	or mapId == 632 or mapId == 668
+	or mapId == 649 or mapId == 650
+	
+	then
+		if instanceId and instanceId > 0 then
+			if not cachedFaction[instanceId] then
+				for _, otherPlayer in pairs(GetPlayersInWorld()) do
+					if otherPlayer:GetInstanceId() == instanceId and player:GetGUIDLow() ~= otherPlayer:GetGUIDLow() then -- Builds cache if there is none, in the event of an ELUNA reload
+						cachedFaction[instanceId] = otherPlayer:GetFaction()
+						if player:GetFaction() ~= cachedFaction[instanceId] then
+							player:SetFaction(cachedFaction[instanceId])
+						end
+						break
+					end
+				end
+				
+				if not cachedFaction[instanceId] then
+					cachedFaction[instanceId] = player:GetFaction()
+				end
+			else
+				if player:GetFaction() ~= cachedFaction[instanceId] then
+					player:SetFaction(cachedFaction[instanceId])
+				end
+			end
+		end
 
     else
         player:SetFactionForRace(player:GetRace())
